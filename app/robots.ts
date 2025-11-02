ad9481c9ec76d7838705b93f7772a4c29b0b4b74
Jeet karena <108061740+JeetKarena@users.noreply.github.com>
2025-11-02 21:24:58 +0530

    Update robots.txt to point to new sitemap route

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: ['/', '/api/og/', '/api/twitter-og/'],
       disallow: [],
     },
-    sitemap: 'https://zfish-devdocs.vercel.app/sitemap.xml',
+    sitemap: 'https://zfish-devdocs.vercel.app/sitemap',
   }
 }
